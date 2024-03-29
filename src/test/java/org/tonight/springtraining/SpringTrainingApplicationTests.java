623cb3c9ae7a16dc3e74154ad344c2ec13034606
13tonight <gurramhaswanth1999@gmail.com>
2024-03-28 19:58:31 -0500

    spring

---
+++ src/test/java/org/tonight/springtraining/SpringTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tonight.springtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
